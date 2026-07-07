dad28cf733b661c21a23dbf352eca88dad63a8d1
amragcia <am.garciam@duocuc.cl>
2026-07-07 14:32:15 -0400

    opcion
    hola

--- ejercicio1_amarogarcia.py
+++ ejercicio1_amarogarcia.py
@@ -1,4 +1,10 @@
 def leer_opcion():
     while True:
         try:
-            opcion = int(input(''))
+            opcion = int(input('Ingrese una opcion 1-6: '))
+            if 1 <= opcion <= 6:
+                return opcion
+            else:
+                print('Opcion en un rango no valido')
+        except ValueError:
+            print('Solo caracteres no numericos')
